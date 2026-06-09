ec56591d8d3f0e7e3a67ec0c63533df024797691
Andy Anderson <andy@clubanderson.com>
2026-06-08 21:44:28 -0400

    fix: click version_option missing version parameter (#1410)
    Bug #221: @click.version_option() without version argument raises
    RuntimeError when user runs --version. Imported __version__ from
    __init__.py and passed to version_option().
    
    Signed-off-by: Andrew Anderson <andy@clubanderson.com>

--- v2/pkg/knowledge/inception.go
+++ v2/pkg/knowledge/inception.go
@@ -1599,9 +1599,11 @@ func buildPyCLI(name string, vision *Fact) string {
 
 import click
 
+from . import __version__
+
 
 @click.group()
-@click.version_option()
+@click.version_option(version=__version__)
 def main():
     """%s"""
     pass
